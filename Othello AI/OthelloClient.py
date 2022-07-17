9fffba10d676fda3986f96ab555bf487ba75e050
Kyle Gerner <kgcoltsfan@gmail.com>
2022-07-17 00:18:05 -0400

    Working AI, but not smart enough yet

---
+++ Othello AI/OthelloClient.py
@@ -0,0 +1,178 @@
+# Kyle Gerner
+# Started 7.15.22
+# Othello AI, client facing
+import os
+from strategy import OthelloStrategy
+import RulesEvaluator as eval
+from RulesEvaluator import BLACK, WHITE, EMPTY, BOARD_DIMENSION
+
+# Escape sequences for terminal color output
+GREEN_COLOR = '\033[92m'
+RED_COLOR = '\033[91m'
+NO_COLOR = '\033[0m'
+HIGHLIGHT = '\u001b[48;5;238m'
+
+# Miscellaneous
+COLUMN_LABELS = list(map(chr, range(65, 65 + BOARD_DIMENSION)))
+
+
+class GameRunner:
+    """Handles overall gameplay"""
+
+    def __init__(self):
+        self.playerPiece = self.getPieceColorInput()
+        self.enemyPiece = eval.opponentOf(self.playerPiece)
+        self.board = [[EMPTY for _ in range(BOARD_DIMENSION)] for __ in range(BOARD_DIMENSION)]
+        self.board[BOARD_DIMENSION // 2][BOARD_DIMENSION // 2 - 1] = BLACK
+        self.board[BOARD_DIMENSION // 2 - 1][BOARD_DIMENSION // 2] = BLACK
+        self.board[BOARD_DIMENSION // 2][BOARD_DIMENSION // 2] = WHITE
+        self.board[BOARD_DIMENSION // 2 - 1][BOARD_DIMENSION // 2 - 1] = WHITE
+        self.ai = OthelloStrategy(self.enemyPiece, self.enemyPiece == BLACK)
+
+    def endGame(self, winner=None):
+        """Ends the game"""
+        textColor = GREEN_COLOR if winner == self.playerPiece else RED_COLOR
+        colorName = self.nameOfPieceColor(winner)
+        if winner:
+            print(f"\n{textColor}{colorName}{NO_COLOR} wins!")
+        else:
+            print("\nThe game ended in a draw!")
+        print("\nThanks for playing!")
+        exit(0)
+
+    def getUserCoordinateInput(self):
+        """Gets a move input from the user"""
+        coord = input("It's your turn, which spot would you like to play? (A1 - %s%d):\t" % (
+            COLUMN_LABELS[-1], BOARD_DIMENSION)).strip().upper()
+        while True:
+            if coord == 'Q':
+                self.endGame()
+            elif coord == 'P':
+                self.printBoard(eval.getValidMoves(self.playerPiece, self.board))
+                print("Your available moves have been highlighted.")
+                coord = input("It's your turn, which spot would you like to play? (A1 - %s%d):\t" % (
+                    COLUMN_LABELS[-1], BOARD_DIMENSION)).strip().upper()
+            elif len(coord) == (2 if BOARD_DIMENSION < 10 else 3) and coord[0] in COLUMN_LABELS and \
+                    int(coord[1]) in range(1, BOARD_DIMENSION + 1):
+                row, col = int(coord[1]) - 1, COLUMN_LABELS.index(coord[0])
+                if eval.isMoveValid(self.playerPiece, row, col, self.board):
+                    return row, col
+                elif eval.isMoveInRange(row, col) and eval.pieceAt(row, col, self.board) != EMPTY:
+                    coord = input(
+                        "That spot is already taken! Please choose a different spot:   ").strip().upper()
+                else:
+                    self.printBoard(eval.getValidMoves(self.playerPiece, self.board))
+                    coord = input("Please choose one of the highlighted spaces:   ").strip().upper()
+            else:
+                coord = input("Please enter a valid move (A1 - %s%d):   " % (
+                    COLUMN_LABELS[-1], BOARD_DIMENSION)).strip().upper()
+
+    def nameOfPieceColor(self, piece):
+        """Gets the name of the color of the given piece"""
+        if piece == BLACK:
+            return "BLACK"
+        elif piece == WHITE:
+            return "WHITE"
+        else:
+            return "EMPTY"
+
+    def textColorOf(self, piece):
+        """Gets the text color of the given piece, or an empty string if no piece given"""
+        if piece == self.playerPiece:
+            return GREEN_COLOR
+        elif piece == self.enemyPiece:
+            return RED_COLOR
+        else:
+            return ""
+
+    def start(self):
+        """Starts the game and handles all basic gameplay functionality"""
+        turn = BLACK
+        self.printBoard()
+        while True:
+            if eval.hasValidMoves(turn, self.board):
+                if turn == self.playerPiece:
+                    row, col = self.getUserCoordinateInput()
+                    eval.playMove(turn, row, col, self.board)
+                    self.printBoard([[row, col]])
+                else:
+                    userInput = input("Press enter for the AI to play.   ").strip().lower()
+                    if userInput == 'q':
+                        self.endGame()
+                    elif userInput == 'p':
+                        self.printBoard(eval.getValidMoves(turn, self.board))
+                        input("AI's available moves have been highlighted. Press enter to continue.")
+                    row, col = self.ai.findBestMove(self.board)
+                    # print(
+                    #     "Note: temporarily broken because I check against user's valid moves instead of AI's")
+                    # row, col = self.getUserCoordinateInput()
+                    eval.playMove(turn, row, col, self.board)
+                    self.printBoard([[row, col]])
+                    print("The AI played in spot %s%d" % (COLUMN_LABELS[col], row + 1))
+            else:
+                print("%s has no valid moves this turn! %s will play again." % (
+                    self.nameOfPieceColor(turn), self.nameOfPieceColor(eval.opponentOf(turn))))
+            isOver, winner = eval.checkGameOver(self.board)
+            if isOver:
+                self.endGame(winner)
+            turn = eval.opponentOf(turn)
+
+    def printBoard(self, highlightedCoordinates=None):
+        """Prints the gameBoard in a human-readable format"""
+        if highlightedCoordinates is None:
+            highlightedCoordinates = []
+        print("\n\t    %s" % " ".join(COLUMN_LABELS))
+        for rowNum in range(BOARD_DIMENSION):
+            print("\t%d%s| " % (rowNum + 1, "" if rowNum > 8 else " "), end='')
+            for colNum in range(BOARD_DIMENSION):
+                piece = eval.pieceAt(rowNum, colNum, self.board)
+                pieceColor = HIGHLIGHT if [rowNum, colNum] in highlightedCoordinates else ''
+                pieceColor += self.textColorOf(piece)
+                print(f"{pieceColor}%s{NO_COLOR} " % piece, end='')
+            if rowNum == BOARD_DIMENSION // 2:
+                print("   %d turns remain." % (eval.numberOfPieceOnBoard(EMPTY, self.board)), end='')
+            print()
+        userScore, aiScore = eval.currentScore(self.playerPiece, self.board)
+        additionalIndent = " " * ((2 + (2 * (BOARD_DIMENSION // 2 - 1))) - (1 if userScore >= 10 else 0))
+        print(f"\t{additionalIndent}{GREEN_COLOR}{userScore}{NO_COLOR} to {RED_COLOR}{aiScore}{NO_COLOR}\n")
+
+    def getPieceColorInput(self):
+        """Gets input from the user to determine which color they will be"""
+        color_input = input("Would you like to be BLACK ('b') or WHITE ('w')?   ").strip().lower()
+        color = BLACK if color_input == 'b' else WHITE
+        if color == BLACK:
+            print(f"You will be BLACK {GREEN_COLOR}{BLACK}{NO_COLOR}!")
+        else:
+            print(f"You will be WHITE {GREEN_COLOR}{WHITE}{NO_COLOR}!")
+        print(f"Your pieces are shown in {GREEN_COLOR}green{NO_COLOR}!")
+        print(f"Enemy pieces are shown in {RED_COLOR}red{NO_COLOR}!")
+        return color
+
+
+def printGameRules():
+    """Gives the user the option to view the rules of the game"""
+    print("\nType 'q' at any move prompt to quit the game.")
+    print("Type 'p' to show the available moves")
+    showRules = input("Would you like to see the rules? (y/n)   ").strip().lower()
+    if showRules == 'q':
+        print("\nThanks for playing!")
+        exit(0)
+    elif showRules == 'y':
+        print("""\n
+        - OBJECTIVE: Have more pieces on the board than the opponent when all spaces are full
+        - TURNS: Black will go first. Each player will take turns placing one piece each turn
+        - GAMEPLAY: Trap enemy pieces between two friendly pieces to convert them to friendly pieces
+        """)
+
+
+def main():
+    """Prompts user for input and creates a new GameRunner"""
+    os.system("")  # allows output coloring for Windows OS
+    print("Welcome to Kyle's Othello AI!")
+    printGameRules()
+    game = GameRunner()
+    game.start()
+
+
+if __name__ == '__main__':
+    main()
--- Othello AI/strategy.py
+++ Othello AI/strategy.py
@@ -1,12 +1,132 @@
 # Kyle Gerner
-# Started 3.18.21
+# Started 7.15.22
 # Contains AI strategy and board manipulation methods
+import math
+import RulesEvaluator as eval
+from RulesEvaluator import EMPTY, BOARD_DIMENSION
+from functools import cmp_to_key
+from collections import defaultdict
 
-class OthelloStrategy(object):
+MAX_DEPTH = 5
+WIN_SCORE = 1000000000
+
+CORNER_COORDINATES = {(0, 0), (0, BOARD_DIMENSION - 1), (BOARD_DIMENSION - 1, 0),
+                      (BOARD_DIMENSION - 1, BOARD_DIMENSION - 1)}
+CORNER_ADJACENT_COORDINATES = {(0, 1), (1, 1), (1, 0),
+                               (0, BOARD_DIMENSION - 2), (1, BOARD_DIMENSION - 2), (1, BOARD_DIMENSION - 1),
+                               (BOARD_DIMENSION - 2, 0), (BOARD_DIMENSION - 2, 1), (BOARD_DIMENSION - 1, 1),
+                               (BOARD_DIMENSION - 1, BOARD_DIMENSION - 2),
+                               (BOARD_DIMENSION - 2, BOARD_DIMENSION - 2),
+                               (BOARD_DIMENSION - 2, BOARD_DIMENSION - 1)}
+
+
+class OthelloStrategy:
     """Where all the calculations are performed to find the best move"""
 
-    def __init__(self):
-        pass
+    def __init__(self, aiColor, aiGoesFirst):
+        self.aiColor = aiColor
+        self.humanColor = eval.opponentOf(aiColor)
+        self.movesPlayed = 0 if aiGoesFirst else 1
 
     def findBestMove(self, board):
-        pass
+        """Gets the best move for the AI on the given board"""
+        self.movesPlayed = BOARD_DIMENSION**2 - eval.numberOfPieceOnBoard(EMPTY, board)
+        return self.minimax(self.aiColor, -math.inf, math.inf, 0, board)[:2]
+
+    def minimax(self, turn, alpha, beta, depth, board):
+        if depth == MAX_DEPTH or depth + self.movesPlayed == BOARD_DIMENSION ** 2:
+            return -1, -1, self.evaluateBoard(board, depth)
+        validMoves = eval.getValidMoves(turn, board)
+        validMoves.sort(key=validMoveSortKey)
+        if len(validMoves) == 0:
+            return self.minimax(eval.opponentOf(turn), alpha, beta, depth, board)
+        if turn == self.aiColor:
+            # maximize
+            highScore = -math.inf
+            bestRow, bestCol = validMoves[0]
+            for row, col in validMoves:
+                boardCopy = self.copyOfBoard(board)  # possible bottleneck
+                eval.playMove(turn, row, col, boardCopy)
+                _, __, score = self.minimax(eval.opponentOf(turn), alpha, beta, depth + 1, boardCopy)
+                if score > highScore:
+                    highScore = score
+                    bestRow = row
+                    bestCol = col
+                alpha = max(alpha, highScore)
+                if alpha >= beta:
+                    break
+            return bestRow, bestCol, highScore
+        else:
+            # minimize
+            lowScore = math.inf
+            bestRow, bestCol = validMoves[0]
+            for row, col in validMoves:
+                boardCopy = self.copyOfBoard(board)  # possible bottleneck
+                eval.playMove(turn, row, col, boardCopy)
+                _, __, score = self.minimax(eval.opponentOf(turn), alpha, beta, depth + 1, boardCopy)
+                if score < lowScore:
+                    lowScore = score
+                    bestRow = row
+                    bestCol = col
+                beta = min(beta, lowScore)
+                if beta <= alpha:
+                    break
+            return bestRow, bestCol, lowScore
+
+    def evaluateBoard(self, board, additionalPiecesPlayed):
+        """Assigns a value to the board state based on how good it is for the AI"""
+        if additionalPiecesPlayed + self.movesPlayed == BOARD_DIMENSION ** 2:
+            aiScore, humanScore = eval.currentScore(self.aiColor, board)
+            if aiScore > humanScore:
+                return WIN_SCORE
+            elif aiScore < humanScore:
+                return -WIN_SCORE
+            else:
+                return 0
+
+        scores = defaultdict(int)
+        for rowIndex in range(BOARD_DIMENSION):
+            for colIndex in range(BOARD_DIMENSION):
+                piece = board[rowIndex][colIndex]
+                score = evaluatePosition(rowIndex, colIndex)
+                scores[piece] += score
+
+        return scores[self.aiColor] - scores[self.humanColor]
+
+
+    def copyOfBoard(self, board):
+        """Returns a copy of the given board"""
+        return list(map(list, board))  # use numpy if this becomes bottleneck
+
+
+def evaluatePosition(row, col):
+    move = row, col
+    if move in CORNER_COORDINATES:
+        return 10
+    elif move in CORNER_ADJACENT_COORDINATES:
+        return -8
+    elif row in [0, BOARD_DIMENSION - 1] or col in [0, BOARD_DIMENSION - 1]:
+        # on outer border
+        return 6
+    elif row in [1, BOARD_DIMENSION - 2] or col in [1, BOARD_DIMENSION - 2]:
+        # one space inside of outer border
+        return -7
+    else:
+        return 0
+
+def validMovesComparator(move1, move2):
+    """
+    Defines a way to sort two possible moves.
+    Penalizes corner-adjacent moves
+    Prioritizes corners first, then edge pieces (unless corner adjacent)
+    """
+    move1Score = evaluatePosition(move1[0], move1[1])
+    move2Score = evaluatePosition(move2[0], move2[1])
+    if move1Score > move2Score:
+        return -1
+    elif move1Score < move2Score:
+        return 1
+    else:
+        return 0
+
+validMoveSortKey = cmp_to_key(validMovesComparator)
