e05b09777740ea009c4ceaa52cf475d08f5e2283
Kyle Gerner <kgcoltsfan@gmail.com>
2022-08-30 01:41:35 -0400

    Working eraseMode functionality

--- Mancala Avalanche/Python Version/MancalaAvalanche.py
+++ Mancala Avalanche/Python Version/MancalaAvalanche.py
@@ -18,22 +18,40 @@ ERASE_MODE_ON = True
 
 # reads in the input for the board from the user
 def inputForBoard():
+    playerValuesInput = input(f"""
+From your left to your right (or top to bottom), enter the # of pebbles in
+each spot on {GREEN_COLOR}your{NO_COLOR} side of the board, with a space separating each number:
+
+    """).strip().split()
+
     while True:
         try:
-            playerVals = [int(item) for item in input("From your left to your right (or top to bottom), enter the # of pebbles in each spot on your side of the board, with a space separating each number:\n").split()]
+            erasePreviousLines(1)
+            playerVals = [int(item) for item in playerValuesInput]
+            if len(playerVals) != 6:
+                playerValuesInput = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
+                continue
+            break
         except ValueError:
-            print(f"{ERROR_SYMBOL} There was an issue with your input. Please try again.")
-            continue
-        if len(playerVals) != 6:
-            print(f"{ERROR_SYMBOL} There should be 6 values entered")
-            continue
-        break
+            playerValuesInput = input(f"{ERROR_SYMBOL} There was an issue with your input. Please try again.\t").strip().split()
+    erasePreviousLines(4)
+
+    enemyValuesInput = input(f"""
+From your left to your right (or top to bottom), enter the # of pebbles in 
+each spot on the {RED_COLOR}enemy{NO_COLOR} side of the board, with a space separating each number:
+
+    """).strip().split()
     while True:
-        enemyVals = [int(item) for item in input("From your left to your right (or top to bottom), enter the # of pebbles in each spot on the enemy side of the board, with a space separating each number:\n").split()]
-        if len(enemyVals) != 6:
-            print(f"{ERROR_SYMBOL} There should be 6 values entered")
-            continue
-        break
+        erasePreviousLines(1)
+        try:
+            enemyVals = [int(item) for item in enemyValuesInput]
+            if len(enemyVals) != 6:
+                enemyValuesInput = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
+                continue
+            break
+        except ValueError:
+            enemyValuesInput = input(f"{ERROR_SYMBOL} There was an issue with your input. Please try again.\t").strip().split()
+    erasePreviousLines(4)
     enemyVals.reverse()
     boardVals = playerVals + [0] + enemyVals + [0]
     return boardVals
@@ -46,11 +64,39 @@ def createSolver(boardVals):
     solver = AvalancheSolver(board)
     return solver
 
+# copy a list and return that list with every value increased by one
+def increaseAllValuesInListByOne(l):
+    returnList = l.copy()
+    for i in range(0, len(l)):
+        returnList[i] += 1
+    return returnList
+
+# prints the best moveset all at once
+def printBestMoveStatus(pointsGained, bestMoves):
+    print("\nThe max # of points you can score on this turn is %d in %d move%s." % (pointsGained, len(bestMoves), "" if pointsGained == 1 else "s"))
+    print("The move set is: " + ", ".join(map(str, increaseAllValuesInListByOne(bestMoves))))
+    print("Note: 1 corresponds to the first (left) spot on your side, 6 corresponds to the last (right) spot")
+
+# print the best moveset one by one
+def printBestMovesOneByOne(pointsGained, bestMoves):
+    print("\nThe max # of points you can score on this turn is %d in %d move%s." % (pointsGained, len(bestMoves), "" if pointsGained == 1 else "s"))
+    print("Note: 1 corresponds to the first (left) spot on your side, 6 corresponds to the last (right) spot")
+    print("Press enter each time to receive the next move. Press q to quit at any time.\n")
+    bestMoveIndexes = increaseAllValuesInListByOne(bestMoves)
+    count = 1
+    for moveIndex in bestMoveIndexes:
+        if input("#%d:  %d%s" % (count, moveIndex, "\n" if ERASE_MODE_ON else "")).strip().lower() == 'q':
+            print("\nThanks for using my Mancala Avalanche solver!\n")
+            exit(0)
+        count += 1
+        erasePreviousLines(2)
+
+
 # prints the player's best moves in the selected mode
 def printSequence(mode, solver, pointsGained, bestMoves):
     if mode == ONE_BY_ONE:
-        solver.printBestMovesOneByOne(pointsGained, bestMoves)
-    else: solver.printBestMoveStatus(pointsGained, bestMoves)
+        printBestMovesOneByOne(pointsGained, bestMoves)
+    else: printBestMoveStatus(pointsGained, bestMoves)
 
 def erasePreviousLines(numLines, overrideEraseMode=False):
     """Erases the specified previous number of lines from the terminal"""
@@ -65,38 +111,45 @@ def main():
         global ERASE_MODE_ON
         ERASE_MODE_ON = False
     print("\nWelcome to Kyle's Mancala Avalanche AI! Written on 7.9.2020")
-    if input("\nWould you like to receive your move set one at a time (as opposed to a printed list)? (y/n): ").strip().lower() == "y":
-        printMode = ONE_BY_ONE
-        print("Moves will be presented one at a time.\n")
-    else:
+    if input("\nWould you like to receive your move set in a printed list (as opposed to one at a time)? (y/n):\t").strip().lower() == "y":
+        erasePreviousLines(1)
         printMode = ALL_AT_ONCE
-        print("Moves will be presented all at once.\n")
+        print("Moves will be presented all at once.")
+    else:
+        erasePreviousLines(1)
+        printMode = ONE_BY_ONE
+        print("Moves will be presented one at a time.")
     boardVals = inputForBoard()
     solver = createSolver(boardVals)
-    print("\nThe starting board looks like this:\n")
+    print("\nThe current board looks like this:\n")
     solver.board.printBoardHorizontal()
-    print("(if this is incorrect, restart the program and make sure the values you entered are correct)")
+    firstIteration = True
     while True:
-        input("Press enter to receive best move set")
-        pointsGained, bestMoves = solver.findBestMove(solver.board, 0)
-        printSequence(printMode, solver, pointsGained, bestMoves)
-        solver.makeMovesOnMoveset(bestMoves, solver.board)
-        print("\nThat's the end of the move set.\n")
-        if input("To see the board after these moves, enter \"y\": ").strip().lower() == "y":
-            solver.board.printBoardHorizontal()
-        if input("If you would not like to continue to the next turn, type 'q':  ").strip().lower() == "q":
+        userInput = input("Press enter to receive best move set, or 'q' to quit.\t").strip().lower()
+        erasePreviousLines(1)
+        if userInput == 'q':
             print("Thanks for playing!")
-            break
+            exit(0)
+        pointsGained, bestMoves = solver.findBestMove(solver.board, 0)
+        solver.makeMovesOnMoveset(bestMoves, solver.board)
+        erasePreviousLines(5 if firstIteration else 11)
+        solver.board.printBoardHorizontal()
+        printSequence(printMode, solver, pointsGained, bestMoves)
+        print("%sThat's the end of the move set.\n" % ("\n" if printMode == ALL_AT_ONCE else ""))
         print("You will now be asked to input the new version of the board")
-        oldEnemyPoints = input("How many points does the enemy have after that turn? ").strip()
+        oldEnemyPoints = input("How many points does the enemy have after their turn?\t").strip()
+        erasePreviousLines(1)
         while not oldEnemyPoints.isdigit():
             oldEnemyPoints = input(f"{ERROR_SYMBOL} Please enter a number: ").strip()
+            erasePreviousLines(1)
+        erasePreviousLines(2)
         oldEnemyPoints = int(oldEnemyPoints)
         boardVals = inputForBoard()
         oldPlayerPoints = solver.board.p1.score
         solver = createSolver(boardVals)
         solver.board.p1.score = oldPlayerPoints
         solver.board.p2.score = oldEnemyPoints
+        firstIteration = False
 
 
 if __name__ == '__main__':
