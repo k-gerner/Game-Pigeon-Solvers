d352baca287a30649bef67def1f2a1f836a432c7
Kyle Gerner <kgcoltsfan@gmail.com>
2022-09-04 00:40:51 -0400

    Correct type hinting

--- Connect 4 AI/Player.py
+++ Connect 4 AI/Player.py
@@ -9,10 +9,10 @@ class Player:
         self.color = color
         self.isAI = isAI
 
-    def getMove(self, board:List[List[str]]) -> Tuple[int, int]:
+    def getMove(self, board:List[List[str]]) -> int:
         """Returns the chosen move for a given board, in [rowIndex, columnIndex] format"""
         print("\n<!> Function 'getMove' has not been implemented.\n"+
               "The program has been terminated.\n" +
               "Please make sure that you have implemented 'getMove' from the Player super class.\n")
         exit(0)
-        return -1, -1 # to satisfy the return type hint warning
+        return -1 # to satisfy the return type hint warning
