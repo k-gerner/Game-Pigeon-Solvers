c4d91dd59b5c1fb4fde70d0b660aa6010940eb20
Kyle Gerner <kgcoltsfan@gmail.com>
2023-05-12 17:35:42 -0400

    grey color

--- util/terminaloutput/colors.py
+++ util/terminaloutput/colors.py
@@ -5,6 +5,7 @@ YELLOW_COLOR = '\033[93m'		  # yellow
 RED_COLOR = '\033[91m'		      # red
 BLUE_COLOR = '\033[36m'		      # blue
 DARK_PURPLE_COLOR = '\033[34m'	  # dark purple
+GREY_COLOR = "\033[90m"      	  # grey
 
 # Background colors
 DARK_GREY_BACKGROUND = '\u001b[48;5;238m'  # dark grey; to make lighter, increase 238 to anything 255 or below
