3af49e9fee28b7caa7733162e3ac09844c9e4e95
Kyle Gerner <kgcoltsfan@gmail.com>
2023-05-12 20:26:23 -0400

    erase function

--- util/terminaloutput/erasing.py
+++ util/terminaloutput/erasing.py
@@ -3,6 +3,11 @@ CURSOR_UP_ONE = '\033[1A'
 ERASE_LINE = '\033[2K'
 
 
-def erasePreviousLines(numLines):
+def erasePreviousLines(num_lines:int):
 	"""Erases the specified previous number of lines from the terminal"""
-	print(f"{CURSOR_UP_ONE}{ERASE_LINE}" * max(numLines, 0), end='')
+	print(f"{CURSOR_UP_ONE}{ERASE_LINE}" * max(num_lines, 0), end='')
+
+
+def erase(output_str:str):
+	"""Erases the number of lines printed by the given string"""
+	erasePreviousLines(output_str.count("\n") + 1)
