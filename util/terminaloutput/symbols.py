0c3714355427b63fcea7a9b821d414331a677dcf
Kyle Gerner <kgcoltsfan@gmail.com>
2024-10-06 18:03:43 -0400

    add warning symbol to util

--- util/terminaloutput/symbols.py
+++ util/terminaloutput/symbols.py
@@ -1,4 +1,5 @@
-from util.terminaloutput.colors import RED_COLOR, BLUE_COLOR, NO_COLOR
+from util.terminaloutput.colors import RED_COLOR, BLUE_COLOR, YELLOW_COLOR, NO_COLOR
 
 ERROR_SYMBOL = f"{RED_COLOR}<!>{NO_COLOR}"
+WARN_SYMBOL = f"{YELLOW_COLOR}<!>{NO_COLOR}"
 INFO_SYMBOL = f"{BLUE_COLOR}<!>{NO_COLOR}"
