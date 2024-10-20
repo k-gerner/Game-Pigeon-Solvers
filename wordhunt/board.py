cec7b672d57dd51759b3d17ef61908e7ee8a151c
Kyle Gerner <kgcoltsfan@gmail.com>
2024-10-19 20:00:48 -0400

    Reformat comments in board class. Add class vars for display

--- wordhunt/board.py
+++ wordhunt/board.py
@@ -1,12 +1,15 @@
 # Kyle Gerner
 # 2.24.2021
 
-# Parent class different representations of a Word Hunt board
 class Board(object):
+	""" Parent class different representations of a Word Hunt board """
+	diagram_output_height = 0
+	row_sizes = None
+	name = None
 
-	# takes in an array of Letter objects and sets lb
-	def __init__(self, lettersArr):
-		self.lb = lettersArr  # lb = letter board
+	def __init__(self, letters_arr):
+		""" takes in an array of Letter objects and sets lb """
+		self.lb = letters_arr  # lb = letter board
 		self.directionDict = {
 			0: self.peekUpperLeft,
 			1: self.peekUp,
@@ -18,53 +21,81 @@ class Board(object):
 			7: self.peekLeft
 		}
 
-	# return a copy of the board as is
 	def copyBoard(self):
-		newArr = []
+		""" return a copy of the board as is """
+		new_arr = []
 		for i in self.lb:
-			newArr.append(i.copyLetter())
-		return self.__class__(newArr)
+			new_arr.append(i.copyLetter())
+		return self.__class__(new_arr)
 
-	# look at letter to the upper left but do not mark as visited
 	def peekUpperLeft(self, pos):
+		""" look at letter to the upper left but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter above but do not mark as visited
 	def peekUp(self, pos):
+		""" look at letter above but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter to the upper right but do not mark as visited
 	def peekUpperRight(self, pos):
+		""" look at letter to the upper right but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter to the right but do not mark as visited
 	def peekRight(self, pos):
+		""" look at letter to the right but do not mark as visited"""
 		raise NotImplementedError
 
-	# look at letter to the lower right but do not mark as visited
 	def peekLowerRight(self, pos):
+		""" look at letter to the lower right but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter below but do not mark as visited
 	def peekDown(self, pos):
+		""" look at letter below but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter to lower left but do not mark as visited
 	def peekLowerLeft(self, pos):
+		""" look at letter to lower left but do not mark as visited """
 		raise NotImplementedError
 
-	# look at letter to the left but do not mark as visited
 	def peekLeft(self, pos):
+		""" look at letter to the left but do not mark as visited"""
 		raise NotImplementedError
 
-	def display_board(self, word, positions):
+	def build_diagram(self, positions, word, word_num):
+		""" returns the string representation of the board """
 		raise NotImplementedError
 
-	# look at letter in specified direction and DO mark as visited, -1 on fail
-	def visitDirection(self, pos, dir):
-		visitedLetter = self.directionDict[dir](pos)  # calls the correct function depending on value of dir
-		if visitedLetter == -1 or visitedLetter.visited:
+	def letter_indices_layout(self):
+		"""
+		returns the string representation the indices of letters on the board;
+		only available for square boards
+		"""
+		raise NotImplementedError
+
+	def board_letters_layout(self):
+		"""
+		returns the string representation of the letters on the board
+		"""
+		raise NotImplementedError
+
+	def visit_direction(self, pos, dir):
+		"""
+		look at letter in specified direction and mark as visited
+		returns -1 on fail
+		"""
+		visited_letter = self.directionDict[dir](pos)  # calls the correct function depending on value of dir
+		if visited_letter == -1 or visited_letter.visited:
 			# if unable to look that direction or the letter was already visited
 			return -1
-		visitedLetter.markVisited()
-		return visitedLetter
+		visited_letter.markVisited()
+		return visited_letter
+
+
+def populate_diagram_squares(size, positions):
+	"""
+	Fills a list of strings representing board tiles' inner text
+	"""
+	squares = ["____"] * size
+	for letter_count, letter_pos in enumerate(positions, 1):
+		left_underscores = "__" if letter_count < 10 else "_"
+		squares[letter_pos] = f"{left_underscores}{letter_count}_"
+	return squares
