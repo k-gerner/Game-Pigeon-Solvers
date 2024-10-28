1287ca20d31ae9e861c36e5a7f30ea4ad2c9f083
Kyle Gerner <kgcoltsfan@gmail.com>
2024-10-27 23:53:21 -0400

    Rename Mancala Avalanche vars/funcs pythonically

--- mancalaavalanche/python/classes.py
+++ mancalaavalanche/python/classes.py
@@ -5,9 +5,11 @@
 class AvalanchePlayer(object):
     def __init__(self):
         self.score = 0
-    def incrementScore(self):
+
+    def increment_score(self):
         self.score += 1
-    def copyPlayer(self):
+
+    def copy_player(self):
         p = AvalanchePlayer()
         p.score = self.score
         return p
@@ -16,61 +18,61 @@ class AvalanchePlayer(object):
 ################################################################################################
 ################################################################################################
 
+
 BANK = 0
 EMPTY_PIT = 1
 PIT_WITH_PIECES = 2
+
 
 # Class that represents the board object.
 class AvalancheBoard(object):
 
     # constructor
-    def __init__(self, pebblesInEach, player1, player2, playerOneTurn):
-        self.pebblesList = pebblesInEach.copy()
+    def __init__(self, pebbles_in_each, player1, player2, player_one_turn):
+        self.pebblesList = pebbles_in_each.copy()
         self.p1 = player1
         self.p2 = player2
-        self.p1Turn = playerOneTurn
+        self.p1Turn = player_one_turn
 
-    # Performs the moves on the board by calling the performMove function for each move given
-    def performMoveSet(self, moveList):
-        continueMoves = True
-        moveNumber = 1
-        for boardSpot in moveList:
-            if not continueMoves:
-                print("Move #%d in the given move set was invalid (previous move did not end in player bank)" % moveNumber)
-                print("The move set was:  " + str(moveList))
+    # Performs the moves on the board by calling the perform_move function for each move given
+    def perform_move_set(self, move_list):
+        continue_moves = True
+        move_number = 1
+        for boardSpot in move_list:
+            if not continue_moves:
+                print("Move #%d in the given move set was invalid (previous move did not end in player bank)" % move_number)
+                print("The move set was:  " + str(move_list))
                 break
-            continueMoves = self.performMove(boardSpot)
-            moveNumber += 1
-        self.switchTurn()
+            continue_moves = self.perform_move(boardSpot)
+            move_number += 1
+        self.switch_turn()
         return self.p1.score, self.p2.score
 
     # Performs the move on the board
-    def performMove(self, position):
-        currBankIndex, enemyBankIndex = self.getBankIndexes()
-        currPlayer = self.p1 if self.p1Turn else self.p2
-        numPebbles = self.pebblesList[position]
+    def perform_move(self, position):
+        curr_bank_index, enemy_bank_index = self.get_bank_indexes()
+        curr_player = self.p1 if self.p1Turn else self.p2
+        num_pebbles = self.pebblesList[position]
         self.pebblesList[position] = 0
-        turnEndedInPlayerBank = False
+        turn_ended_in_player_bank = False
         while True:
-            if numPebbles == 0:
-                endOfMove = self.endOfCurrentMove(position, currBankIndex)
-                # if (endOfMove[0]):
-                if(endOfMove != PIT_WITH_PIECES):
-                    # turnEndedInPlayerBank = endOfMove[1]
-                    turnEndedInPlayerBank = True if endOfMove == BANK else False
+            if num_pebbles == 0:
+                end_of_move = self.end_of_current_move(position, curr_bank_index)
+                if end_of_move != PIT_WITH_PIECES:
+                    turn_ended_in_player_bank = True if end_of_move == BANK else False
                     break
                 else:
-                    numPebbles = self.pebblesList[position]
+                    num_pebbles = self.pebblesList[position]
                     self.pebblesList[position] = 0
-            position = (position + 1) % 14 if (position + 1) != enemyBankIndex else (position + 2) % 14
-            self.addPebbleToLocation(position, currBankIndex, currPlayer)
-            numPebbles -= 1
-        return turnEndedInPlayerBank
+            position = (position + 1) % 14 if (position + 1) != enemy_bank_index else (position + 2) % 14
+            self.add_pebble_to_location(position, curr_bank_index, curr_player)
+            num_pebbles -= 1
+        return turn_ended_in_player_bank
 
     # checks which spot the last piece was placed
-    def endOfCurrentMove(self, pos, currBankIndex):
+    def end_of_current_move(self, pos, curr_bank_index):
         # note if this method is called, we already know numPebbles = 0
-        if pos == currBankIndex:
+        if pos == curr_bank_index:
             return BANK
         elif self.pebblesList[pos] == 1:
             return EMPTY_PIT
@@ -78,57 +80,58 @@ class AvalancheBoard(object):
             return PIT_WITH_PIECES
 
     # places a piece in the specified spot, and increments score if applicable
-    def addPebbleToLocation(self, index, currBankIndex, currPlayer):
+    def add_pebble_to_location(self, index, curr_bank_index, curr_player):
         self.pebblesList[index] += 1
-        if index == currBankIndex:
-            currPlayer.incrementScore()
+        if index == curr_bank_index:
+            curr_player.increment_score()
 
     # get the player and opponent bank indexes
-    def getBankIndexes(self):
+    def get_bank_indexes(self):
         if self.p1Turn:
-            return 6,13
+            return 6, 13
         else:
-            return 13,6
+            return 13, 6
 
     # prints the board in a horizontal fashion
-    def printBoardHorizontal(self):
-        # E  |12 |11 |10 | 9 | 8 | 7 |
-        #13  -------------------------  6
-        #    | 0 | 1 | 2 | 3 | 4 | 5 |  P
-        #     Enemy winning 13 to 6
+    def print_board_horizontal(self):
+        #  E  |12 |11 |10 | 9 | 8 | 7 |
+        # 13  -------------------------  6
+        #     | 0 | 1 | 2 | 3 | 4 | 5 |  P
+        #      Enemy winning 13 to 6
         if self.p2.score == self.p1.score:
-            scoreStr = "\t  The score is tied at %d\n" % self.p2.score
+            score_str = "\t  The score is tied at %d\n" % self.p2.score
         else:
             if self.p2.score > self.p1.score:
-                scoreStr = "\t  Enemy winning %d to %d\n" % (self.p2.score, self.p1.score)
+                score_str = "\t  Enemy winning %d to %d\n" % (self.p2.score, self.p1.score)
             else:
-                scoreStr = "\t  You're winning %d to %d\n" % (self.p1.score, self.p2.score)
-        print(str(self) + scoreStr)
+                score_str = "\t  You're winning %d to %d\n" % (self.p1.score, self.p2.score)
+        print(str(self) + score_str)
 
     # string representation of the board
     def __str__(self):
-        enemyRow = "E\t|" + self.scoreRowToStrHoriz(12, 6, -1) + "\n"
-        bankRow = "%d\t-------------------------\t%d\n" % (self.p2.score, self.p1.score)
-        playerRow = "\t|" + self.scoreRowToStrHoriz(0, 6, 1) + "\tP\n"
-        return enemyRow + bankRow + playerRow
+        enemy_row = "E\t|" + self.score_row_to_str_horiz(12, 6, -1) + "\n"
+        bank_row = "%d\t-------------------------\t%d\n" % (self.p2.score, self.p1.score)
+        player_row = "\t|" + self.score_row_to_str_horiz(0, 6, 1) + "\tP\n"
+        return enemy_row + bank_row + player_row
 
     # string representation of one side of the board
-    def scoreRowToStrHoriz(self, start, end, direction):
-        scoresStr = ""
-        for i in range(start, end, direction): # loop thru indexes of side
-            thisSpotStr = "%d |" % self.pebblesList[i]
+    def score_row_to_str_horiz(self, start, end, direction):
+        scores_str = ""
+        for i in range(start, end, direction):  # loop thru indexes of side
+            this_spot_str = "%d |" % self.pebblesList[i]
             if self.pebblesList[i] < 10:
-                thisSpotStr = " " + thisSpotStr
-            scoresStr += thisSpotStr
-        return scoresStr
+                this_spot_str = " " + this_spot_str
+            scores_str += this_spot_str
+        return scores_str
 
     # switches whose turn it is
-    def switchTurn(self):
+    def switch_turn(self):
         self.p1Turn = not self.p1Turn
 
 ################################################################################################
 ################################################################################################
 ################################################################################################
+
 
 # Class that contains the methods that calculate the best moves for a given board
 class AvalancheSolver(object):
@@ -138,54 +141,51 @@ class AvalancheSolver(object):
         self.board = board
 
     # Returns a copy of the game board
-    def copyBoard(self, boardToCopy):
-        return AvalancheBoard(boardToCopy.pebblesList, boardToCopy.p1.copyPlayer(), boardToCopy.p2.copyPlayer(), boardToCopy.p1Turn)
+    def copy_board(self, board_to_copy):
+        return AvalancheBoard(board_to_copy.pebblesList, board_to_copy.p1.copy_player(), board_to_copy.p2.copy_player(), board_to_copy.p1Turn)
 
     # Performs the moves of a given moveset on the given board
-    def makeMovesOnMoveset(self, moveList, board):
-        prevScore = board.p1.score
-        board.performMoveSet(moveList)
-        return board.p1.score - prevScore
+    def make_moves_on_moveset(self, move_list, board):
+        prev_score = board.p1.score
+        board.perform_move_set(move_list)
+        return board.p1.score - prev_score
 
     # Perform a single move on a given board
     # returns the score for this turn, and whether or not the turn ended in the player's bank
-    def makeMove(self, index, board):
-        prevScore = board.p1.score
-        endedInBank = board.performMove(index)
-        return board.p1.score - prevScore, endedInBank
+    def make_move(self, index, board):
+        prev_score = board.p1.score
+        ended_in_bank = board.perform_move(index)
+        return board.p1.score - prev_score, ended_in_bank
 
     # Recursive method that finds the best move for the player for a given board
     # returns the points gained from a moveset, and the moveset list
-    def findBestMove(self, board, currVal):
-        indexOptions = self.getListOfNonZeroIndexes(board)
-        if len(indexOptions) == 0:
+    def find_best_move(self, board, curr_val):
+        index_options = self.get_list_of_non_zero_indexes(board)
+        if len(index_options) == 0:
             # if no available moves
-            return currVal, []
-        bestIncrease = -1
-        bestIndex = indexOptions[0]
-        bestMoveList = [0]
+            return curr_val, []
+        best_increase = -1
+        best_move_list = [0]
         # loop through each available move
-        for index in indexOptions:
-            thisRunIncrease = 0
-            thisMoveList = []
-            boardCopy = self.copyBoard(board)
-            makeMoveResults = self.makeMove(index, boardCopy)
-            pointsGained, endedInBank = makeMoveResults[0], makeMoveResults[1]
-            if endedInBank:
-                thisRunIncrease, thisMoveList = self.findBestMove(boardCopy, pointsGained)
+        for index in index_options:
+            this_move_list = []
+            board_copy = self.copy_board(board)
+            make_move_results = self.make_move(index, board_copy)
+            points_gained, ended_in_bank = make_move_results[0], make_move_results[1]
+            if ended_in_bank:
+                this_run_increase, this_move_list = self.find_best_move(board_copy, points_gained)
             else:
-                thisRunIncrease = pointsGained
-            if thisRunIncrease > bestIncrease:
-                bestIncrease = thisRunIncrease
-                bestIndex = index
-                bestMoveList = thisMoveList.copy()
-                bestMoveList.insert(0, bestIndex)
-        return bestIncrease + currVal, bestMoveList
+                this_run_increase = points_gained
+            if this_run_increase > best_increase:
+                best_increase = this_run_increase
+                best_move_list = this_move_list.copy()
+                best_move_list.insert(0, index)
+        return best_increase + curr_val, best_move_list
 
     # get a list of the indexes that have pieces in them (and therefore are available to be played)
-    def getListOfNonZeroIndexes(self, board):
-        nonZeros = []
+    def get_list_of_non_zero_indexes(self, board):
+        non_zeros = []
         for i in range(0, 6, 1):
             if board.pebblesList[i] != 0:
-                nonZeros.append(i)
-        return nonZeros
+                non_zeros.append(i)
+        return non_zeros
--- mancalaavalanche/python/mancala_ava_client.py
+++ mancalaavalanche/python/mancala_ava_client.py
@@ -9,9 +9,9 @@ ONE_BY_ONE = 1
 ALL_AT_ONCE = 2
 
 
-def inputForBoard():
+def input_for_board():
 	"""Reads in the input for the board from the user"""
-	playerValuesInput = input(f"""
+	player_values_input = input(f"""
 From your left to your right (or top to bottom), enter the # of pebbles in
 each spot on {GREEN_COLOR}your{NO_COLOR} side of the board, with a space separating each number:
 
@@ -20,17 +20,17 @@ each spot on {GREEN_COLOR}your{NO_COLOR} side of the board, with a space separat
 	while True:
 		try:
 			erasePreviousLines(1)
-			playerVals = [int(item) for item in playerValuesInput]
-			if len(playerVals) != 6:
-				playerValuesInput = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
+			player_vals = [int(item) for item in player_values_input]
+			if len(player_vals) != 6:
+				player_values_input = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
 				continue
 			break
 		except ValueError:
-			playerValuesInput = input(
+			player_values_input = input(
 				f"{ERROR_SYMBOL} There was an issue with your input. Please try again.\t").strip().split()
 	erasePreviousLines(4)
 
-	enemyValuesInput = input(f"""
+	enemy_values_input = input(f"""
 From your left to your right (or top to bottom), enter the # of pebbles in 
 each spot on the {RED_COLOR}enemy{NO_COLOR} side of the board, with a space separating each number:
 
@@ -38,54 +38,51 @@ each spot on the {RED_COLOR}enemy{NO_COLOR} side of the board, with a space sepa
 	while True:
 		erasePreviousLines(1)
 		try:
-			enemyVals = [int(item) for item in enemyValuesInput]
-			if len(enemyVals) != 6:
-				enemyValuesInput = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
+			enemy_vals = [int(item) for item in enemy_values_input]
+			if len(enemy_vals) != 6:
+				enemy_values_input = input(f"{ERROR_SYMBOL} There should be 6 values entered.\t").strip().split()
 				continue
 			break
 		except ValueError:
-			enemyValuesInput = input(
+			enemy_values_input = input(
 				f"{ERROR_SYMBOL} There was an issue with your input. Please try again.\t").strip().split()
 	erasePreviousLines(4)
-	enemyVals.reverse()
-	boardVals = playerVals + [0] + enemyVals + [0]
-	return boardVals
+	enemy_vals.reverse()
+	board_vals = player_vals + [0] + enemy_vals + [0]
+	return board_vals
 
 
-def createSolver(boardVals):
+def create_solver(board_vals):
 	"""Creates the AvalancheSolver object"""
 	p1 = AvalanchePlayer()
 	p2 = AvalanchePlayer()
-	board = AvalancheBoard(boardVals, p1, p2, True)
+	board = AvalancheBoard(board_vals, p1, p2, True)
 	solver = AvalancheSolver(board)
 	return solver
 
 
-def increaseAllValuesInListByOne(l):
+def increase_all_values_in_list_by_one(values_list):
 	"""Copy a list and return that list with every value increased by one"""
-	returnList = l.copy()
-	for i in range(0, len(l)):
-		returnList[i] += 1
-	return returnList
+	return [item + 1 for item in values_list]
 
 
-def printBestMoveStatus(pointsGained, bestMoves):
+def print_best_move_status(points_gained, best_moves):
 	"""Prints the best moveset all at once"""
 	print("\nThe max # of points you can score on this turn is %d in %d move%s." % (
-	pointsGained, len(bestMoves), "" if pointsGained == 1 else "s"))
-	print("The move set is: " + ", ".join(map(str, increaseAllValuesInListByOne(bestMoves))))
+		points_gained, len(best_moves), "" if points_gained == 1 else "s"))
+	print("The move set is: " + ", ".join(map(str, increase_all_values_in_list_by_one(best_moves))))
 	print("Note: 1 corresponds to the first (left) spot on your side, 6 corresponds to the last (right) spot")
 
 
-def printBestMovesOneByOne(pointsGained, bestMoves):
+def print_best_moves_one_by_one(points_gained, best_moves):
 	"""Prints the best moveset one by one"""
 	print("\nThe max # of points you can score on this turn is %d in %d move%s." % (
-	pointsGained, len(bestMoves), "" if pointsGained == 1 else "s"))
+		points_gained, len(best_moves), "" if points_gained == 1 else "s"))
 	print("Note: 1 corresponds to the first (left) spot on your side, 6 corresponds to the last (right) spot")
 	print("Press enter each time to receive the next move. Press q to quit at any time.\n")
-	bestMoveIndexes = increaseAllValuesInListByOne(bestMoves)
+	best_move_indexes = increase_all_values_in_list_by_one(best_moves)
 	count = 1
-	for moveIndex in bestMoveIndexes:
+	for moveIndex in best_move_indexes:
 		if input("#%d:  %d%s" % (count, moveIndex, "\n")).strip().lower() == 'q':
 			print("\nThanks for using my Mancala Avalanche solver!\n")
 			exit(0)
@@ -93,12 +90,12 @@ def printBestMovesOneByOne(pointsGained, bestMoves):
 		erasePreviousLines(2)
 
 
-def printSequence(mode, pointsGained, bestMoves):
+def print_sequence(mode, points_gained, best_moves):
 	"""Prints the player's best moves in the selected mode"""
 	if mode == ONE_BY_ONE:
-		printBestMovesOneByOne(pointsGained, bestMoves)
+		print_best_moves_one_by_one(points_gained, best_moves)
 	else:
-		printBestMoveStatus(pointsGained, bestMoves)
+		print_best_move_status(points_gained, best_moves)
 
 
 def run():
@@ -106,40 +103,40 @@ def run():
 	if input(
 			"\nWould you like to receive your move set in a printed list (as opposed to one at a time)? (y/n):\t").strip().lower() == "y":
 		erasePreviousLines(1)
-		printMode = ALL_AT_ONCE
+		print_mode = ALL_AT_ONCE
 		print("Moves will be presented all at once.")
 	else:
 		erasePreviousLines(1)
-		printMode = ONE_BY_ONE
+		print_mode = ONE_BY_ONE
 		print("Moves will be presented one at a time.")
-	boardVals = inputForBoard()
-	solver = createSolver(boardVals)
+	board_vals = input_for_board()
+	solver = create_solver(board_vals)
 	print("\nThe current board looks like this:\n")
-	solver.board.printBoardHorizontal()
-	firstIteration = True
+	solver.board.print_board_horizontal()
+	first_iteration = True
 	while True:
-		userInput = input("Press enter to receive best move set, or 'q' to quit.\t").strip().lower()
+		user_input = input("Press enter to receive best move set, or 'q' to quit.\t").strip().lower()
 		erasePreviousLines(1)
-		if userInput == 'q':
+		if user_input == 'q':
 			print("Thanks for playing!")
 			exit(0)
-		pointsGained, bestMoves = solver.findBestMove(solver.board, 0)
-		solver.makeMovesOnMoveset(bestMoves, solver.board)
-		erasePreviousLines(5 if firstIteration else 11)
-		solver.board.printBoardHorizontal()
-		printSequence(printMode, pointsGained, bestMoves)
-		print("%sThat's the end of the move set.\n" % ("\n" if printMode == ALL_AT_ONCE else ""))
+		points_gained, best_moves = solver.find_best_move(solver.board, 0)
+		solver.make_moves_on_moveset(best_moves, solver.board)
+		erasePreviousLines(5 if first_iteration else 11)
+		solver.board.print_board_horizontal()
+		print_sequence(print_mode, points_gained, best_moves)
+		print("%sThat's the end of the move set.\n" % ("\n" if print_mode == ALL_AT_ONCE else ""))
 		print("You will now be asked to input the new version of the board")
-		oldEnemyPoints = input("How many points does the enemy have after their turn?\t").strip()
+		old_enemy_points = input("How many points does the enemy have after their turn?\t").strip()
 		erasePreviousLines(1)
-		while not oldEnemyPoints.isdigit():
-			oldEnemyPoints = input(f"{ERROR_SYMBOL} Please enter a number: ").strip()
+		while not old_enemy_points.isdigit():
+			old_enemy_points = input(f"{ERROR_SYMBOL} Please enter a number: ").strip()
 			erasePreviousLines(1)
 		erasePreviousLines(2)
-		oldEnemyPoints = int(oldEnemyPoints)
-		boardVals = inputForBoard()
-		oldPlayerPoints = solver.board.p1.score
-		solver = createSolver(boardVals)
-		solver.board.p1.score = oldPlayerPoints
-		solver.board.p2.score = oldEnemyPoints
-		firstIteration = False
+		old_enemy_points = int(old_enemy_points)
+		board_vals = input_for_board()
+		old_player_points = solver.board.p1.score
+		solver = create_solver(board_vals)
+		solver.board.p1.score = old_player_points
+		solver.board.p2.score = old_enemy_points
+		first_iteration = False
